2e22e415d374f79abf8760eca66f0edb045b2adb
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-02 17:11:28 -0500

    [JB] extract query to own file, upgrade imperion system

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,46 +1,8 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { components, themes } from '@sparkimaginations/imperion-system';
+import { FEED_QUERY } from '../queries';
 
-
-const FEED_QUERY = gql`
-query {
-  bannerCollection(limit: 5, order: sys_firstPublishedAt_ASC) {
-    items {
-      heading
-      buttonText
-      body
-      theme
-    }
-  }
-  blogPostCollection(limit: 5, order: publishDate_DESC)  {
-    items {
-      title
-      author {
-        name
-      }
-      body
-      publishDate
-      theme
-    }
-  }
-  featuredQuoteCollection(limit: 5) {
-    items {
-      alt
-      caption
-      heading
-      human
-      quote
-      subtext
-      theme
-      title
-      image {
-        url
-      }
-    }
-  }
-}
-`
 const MainPage = () => {
     const data = useQuery(FEED_QUERY).data;
     const refs = [
@@ -67,23 +29,22 @@ const MainPage = () => {
 
     return (
         <div>
-            {data && data.bannerCollection && data.bannerCollection.items.map((obj, index) => {
+            {data && data["bannerCollection"] && data["bannerCollection"].items.map((obj, index) => {
                 const myObj = {...obj}
                 myObj.theme = themes[obj.theme];
                 return <Banner key={index} {...myObj} ref={refs[index]}/>
             })}
-            {data && data.blogPostCollection && data.blogPostCollection.items.map((obj, index) => {
+            {data && data["blogPostCollection"] && data["blogPostCollection"].items.map((obj, index) => {
                 const myObj = {...obj}
                 myObj.author = obj.author.name;
                 myObj.theme = themes[obj.theme];
                 return <BlogPost key={index} {...myObj} ref={refs[index + 5]}/>
             })}
-            {data && data.featuredQuoteCollection && data.featuredQuoteCollection.items.map((obj, index) => {
+            {data && data["featuredQuoteCollection"] && data["featuredQuoteCollection"].items.map((obj, index) => {
                 const myObj = {...obj}
                 myObj.url = obj.image.url;
                 myObj.theme = themes[obj.theme];
                 return <FeaturedQuote key={index} {...myObj} ref={refs[index+10]} />;
-
             })}
         </div>
     );
---
+++ src/queries.js
@@ -0,0 +1,40 @@
+import { gql } from "@apollo/client";
+
+export const FEED_QUERY = gql`
+query {
+  bannerCollection(limit: 5, order: sys_firstPublishedAt_ASC) {
+    items {
+      heading
+      buttonText
+      body
+      theme
+    }
+  }
+  blogPostCollection(limit: 5, order: publishDate_DESC)  {
+    items {
+      title
+      author {
+        name
+      }
+      body
+      publishDate
+      theme
+    }
+  }
+  featuredQuoteCollection(limit: 5) {
+    items {
+      alt
+      caption
+      heading
+      human
+      quote
+      subtext
+      theme
+      title
+      image {
+        url
+      }
+    }
+  }
+}
+`
