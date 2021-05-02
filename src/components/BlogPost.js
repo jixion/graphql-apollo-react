0e742f1112e88f42b80b17e006637666cbf761c5
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-02 05:11:03 -0500

    [JB] format blog post

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,14 +4,19 @@ import { tokens } from '@sparkimaginations/imperion-system/';
 import ReactMarkdown from 'react-markdown';
 
 const StyledBlogPost = styled.div`
-    padding: ${tokens.spacers.spacer40};
+    padding: ${tokens.spacers.spacer40} ${tokens.spacers.spacer20};
     color: ${tokens.colors.kleinBlue};
     background-color: ${tokens.colors.black};
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 const Header = styled.div`
     background-color: #020021;
+    width: 500px;
+    margin-bottom: ${tokens.spacers.spacer10}
 `;
 
 const PublishDate = styled.span`
@@ -29,6 +34,9 @@ const Author = styled.p`
 const Body = styled(ReactMarkdown)`
     padding: ${tokens.spacers.spacer50};
     background-color: #020021;
+    text-align: left;
+    width: 800px;
+    overflow: scroll;
 `;
 
 const BlogPost = (props) => {
