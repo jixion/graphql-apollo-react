cbbd92bd054bfb1bd3a0aa36e45f51332ee3b2a9
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-06 15:42:22 -0500

    [JB] change graphql endpoint

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,55 +1,28 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { components, themes } from '@sparkimaginations/imperion-system';
-import { FEED_QUERY, HEROKU } from '../queries';
+import { HEROKU } from '../queries';
 
 const MainPage = () => {
-    const data = [];// = useQuery(FEED_QUERY).data;
-    const myGraphQL = useQuery(HEROKU).data;
-    const refs = [
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef(),
-        React.useRef()
-    ];
-    const Banner = components.modules.Banner;
-    const FeaturedQuote = components.modules.FeaturedQuote;
-    const BlogPost = components.modules.BlogPost;
+    const data = useQuery(HEROKU).data;
+    let apod = data["apod"];
 
+    // copyright
+    // date
+    // explanation
+    // hdurl
+    // media_type
+    // service_version
+    // title
+    // url
     return (
         <div>
-            {myGraphQL && myGraphQL["locations"] && myGraphQL.locations.map((loc, i)=>(<span key={i}>{loc.name}<br /></span>))}
-            {data && data["bannerCollection"] && data["bannerCollection"].items.map((obj, index) => {
-                const myObj = {...obj}
-                myObj.theme = themes[obj.theme];
-                return <Banner key={index} {...myObj} ref={refs[index]}/>
-            })}
-            {data && data["blogPostCollection"] && data["blogPostCollection"].items.map((obj, index) => {
-                const myObj = {...obj}
-                myObj.author = obj.author.name;
-                myObj.theme = themes[obj.theme];
-                return <BlogPost key={index} {...myObj} ref={refs[index + 5]}/>
-            })}
-            {data && data["featuredQuoteCollection"] && data["featuredQuoteCollection"].items.map((obj, index) => {
-                const myObj = {...obj}
-                myObj.url = obj.image.url;
-                myObj.theme = themes[obj.theme];
-                return <FeaturedQuote key={index} {...myObj} ref={refs[index+10]} />;
-            })}
+            {apod && <div>
+                <h1>{apod.title}</h1>
+                <p>{apod.explanation}</p>
+                <img src={apod.hdurl} alt={apod.explanation} />
+            </div>}
         </div>
     );
-};
+}
 
 export default MainPage;
--- src/queries.js
+++ src/queries.js
@@ -3,45 +3,17 @@ import { gql } from "@apollo/client";
 export const HEROKU = gql`
 query {
     locations {
+        id
         name
+    }
+    apod {
+        copyright
+        date
+        explanation
+        hdurl
+        media_type
+        service_version
+        title
+        url
     }
 }`
-
-export const FEED_QUERY = gql`
-query {
-  bannerCollection(limit: 5, order: sys_firstPublishedAt_ASC) {
-    items {
-      heading
-      buttonText
-      body
-      theme
-    }
-  }
-  blogPostCollection(limit: 5, order: publishDate_DESC)  {
-    items {
-      title
-      author {
-        name
-      }
-      body
-      publishDate
-      theme
-    }
-  }
-  featuredQuoteCollection(limit: 5) {
-    items {
-      alt
-      caption
-      heading
-      human
-      quote
-      subtext
-      theme
-      title
-      image {
-        url
-      }
-    }
-  }
-}
-`
