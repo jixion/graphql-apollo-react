b5b36c73a5b52f42d2967450a848e81caf3a8ec1
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-16 04:39:10 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Layout from "./layouts";
 
 function IndexPage() {
-    return <Layout><h1>Check this shiz out!</h1></Layout>
+    return <Layout><h1>Check this shiz out!  I've got my own components installed and running.  We're using React and Styled Components.  We're also using graphql and apollo now.  Just today 16 May 21 we added routing via nextjs.  This has turned into a cool project!</h1></Layout>
 }
 
 export default IndexPage;
